b4321f3c4cbbf19735881f8962f363a5fe3bf5d5
Dev Namdev <devnamdevcse@gmail.com>
2023-01-05 14:38:14 +0530

    django deploy

--- dev/settings.py
+++ dev/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from django.contrib import messages
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # mannualy
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
